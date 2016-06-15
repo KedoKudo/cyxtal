53abb611daaba7693ea3754d0dc38c3fe0219d19
Chen <chenzhang8722@gmail.com>
2016-06-15 16:56:03 -0400

    migrate Point class from ezxtal to cyxtal

--- geometry.py
+++ geometry.py
@@ -35,3 +35,105 @@ DESCRIPTION
 -----------
 Standard geometry class for 2D/3D geometry calculation.
 """
+
+import numpy as np
+
+
+class Point(object):
+    """
+    DESCRIPTION
+    -----------
+    Point(x,y,z)
+        Point in 3D space, base class provide bare bone abstraction
+        for point related calculation.
+    PARAMETERS
+    ----------
+    x,y,z: float
+        Standard Cartesian coordinates for location description.
+    METHODS
+    -------
+    dist2point(Point other)
+        Return the distance to another instance of Point.
+    dist2line(Line other)
+        Return the distance to given instance of Line.
+    on_line(Line other)
+        Whether the current instance lies on a given instance of Line.
+    in_plane(Plane other)
+        Whether the current point lies in a given instance of Plane.
+    CLASSMETHOD
+    -----------
+    """
+    def __init__(self, x, y, z):
+        self._coord = [x, y, z]
+
+    @property
+    def x(self):
+        return self._coord[0]
+
+    @x.setter
+    def x(self, val):
+        self._coord[0] = val
+
+    @property
+    def y(self):
+        return self._coord[1]
+
+    @y.setter
+    def y(self, val):
+        self._coord[1] = val
+
+    @property
+    def z(self):
+        return self._coord[2]
+
+    @z.setter
+    def z(self, val):
+        self._coord[2] = val
+
+    @property
+    def coord(self):
+        return self._coord
+
+    @coord.setter
+    def coord(self, val):
+        if len(val) != 3:
+            raise ValueError("Need 3 coordinates")
+        self._coord = val
+
+    def __str__(self):
+        """String representation of Point"""
+        return "(" + str(self._coord)[1: -1] + ")"
+
+    def __eq__(self, other):
+        if np.absolute(self.x - other.x) < 1e-6:
+            if np.absolute(self.y - other.y) < 1e-6:
+                if np.absolute(self.z - other.z) < 1e-6:
+                    return True
+        return False
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __len__(self):
+        return 3
+
+    def dist2point(self, other):
+        """Return the distance to another point"""
+        distance = (self.x - other.x)**2 + \
+                   (self.y - other.y)**2 + \
+                   (self.z - other.z)**2
+        return np.sqrt(distance)
+
+    def dist2line(self, line):
+        """Return the distance to another line"""
+        return line.dist2point(self)
+
+    def on_line(self, line):
+        """Quick test is the point is on the given line"""
+        return line.contain_point(self)
+
+    def in_plane(self, plane):
+        """Quick test if a point is in a given plane"""
+        return plane.contain_point(self)
+
+
