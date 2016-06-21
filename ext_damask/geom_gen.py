7fe3cdbe8e9f0bfa1082ecea42b9de816e7bfa3b
Chen <chenzhang8722@gmail.com>
2016-06-21 11:20:20 -0400

    initial parsing of rcb file and testing skeloton

--- ext_damask/geom_gen.py
+++ ext_damask/geom_gen.py
@@ -61,15 +61,16 @@ def geom_fromRCB(rcbFile,
                  viz=None,
                  rim=1,
                  thickness=1,
-                 step=1):
+                 step=1,
+                 debug=False):
     """
     DESCRIPTION
     -----------
-    geom, textures = parse_rcb(RCBFILE,
-                               output='GEOM_FILE_NAME',
-                               rim=RIM_SIZE,
-                               thickness=NUM_VOXEL_Z_DIRECTION,
-                               step=MESH_RESOLUTION_IN_um)
+    geom, textures = geom_fromRCB(RCBFILE,
+                                  output='GEOM_FILE_NAME',
+                                  rim=RIM_SIZE,
+                                  thickness=NUM_VOXEL_Z_DIRECTION,
+                                  step=MESH_RESOLUTION_IN_um)
         Generate a columnar microstructure based on given surface OIM
     data (RCBFILE: reconstructed boundary file), optional geom file and
     material configuration file can be auto generated during the process.
@@ -86,12 +87,63 @@ def geom_fromRCB(rcbFile,
         Thickness along the sample z direction (depth). Since no subsurface
         information is available, the thickness of each grain is arbitrarily
         defined through this option.
+    step:      int
+        Spectral mesh resolution. Default value is set to 1, i.e. the smallest
+        representation of the microstructure is a 1x1x1 um box.
+    debug:     boolean
+        Control debugging output to terminal.
     RETURNS
     -------
+    geom:      numpy.array
+        A 3D numpy array consists of microstructure IDs (grain ID).
+    textures:  dict
+        A dictionary keep tracks of grain ID and the corresponding crystal
+        orientation in Bunge Euler angles.
     NOTES
+    -----
+    Standard header of the reconstructed grain boundary file from TSL software
+    # Column 1-3:   right hand average orientation (phi1, PHI, phi2 in radians)
+    # Column 4-6:   left hand average orientation (phi1, PHI, phi2 in radians)
+    # Column 7:     length (in microns)
+    # Column 8:     trace angle (in degrees)
+    # Column 9-12:  x,y coordinates of endpoints (in microns)
+    # Column 13-14: IDs of right hand and left hand grains
     ----
     """
-    pass
+    # textures = {ID: orientation}
+    # grainID=1 is reserved for rim.
+    textures = {}
+    textures[1] = 'rim'
+    # grains = {ID: [vtx]}
+    grains = {}
+    grains[1] = []
+    with open(rcbFile) as f:
+        # remove header
+        rawstr = f.readlines()[8:]
+        for line in rawstr:
+            tmpData = [float(item) for item in line.split()]
+            l_O = tuple(np.rad2deg(tmpData[0:3]))
+            r_O = tuple(np.rad2deg(tmpData[3:6]))
+            vtx1 = Point2D(tmpData[8], tmpData[9])
+            vtx2 = Point2D(tmpData[10], tmpData[11])
+            l_gid = int(tmpData[13]) + 1
+            r_gid = int(tmpData[12]) + 1
+            # parse left grain
+            if l_gid in textures.keys():
+                grains[l_gid].append(vtx1)
+                grains[l_gid].append(vtx2)
+            else:
+                textures[l_gid] = l_O
+                grains[l_gid] = [vtx1, vtx2]
+            # parse right grain
+            if r_gid in textures.keys():
+                grains[r_gid].append(vtx1)
+                grains[r_gid].append(vtx2)
+            else:
+                textures[r_gid] = r_O
+                grains[r_gid] = [vtx1, vtx2]
+    # generate some debugging info
+
 
 
 def geom_fromSeed():
