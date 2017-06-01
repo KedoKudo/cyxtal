7a1c62a8696208a852608293b8f379e7ac40d000
Chen <chenzhang8722@gmail.com>
2017-06-01 11:31:25 -0400

    adding a few testing statements
    help test whether we should switch to deformation gradient based strain refinement

--- development/strain_refine_check.py
+++ development/strain_refine_check.py
@@ -10,3 +10,32 @@
 #    strain tensor
 
 import numpy as np
+from scipy.linalg import polar
+from scipy.optimize import minimize
+from cyxtal.ext_aps.parsers import F2DeviatoricStrain
+from cyxtal.ext_aps.parsers import get_base
+
+
+def gen_distorsion():
+  """ generate random distorsion in the form of deformation gradient """
+  F = np.random.random((3,3))
+  R, U = polar(F, side='right')  # no rotation
+
+  print 3-np.trace(R)
+  return U, F2DeviatoricStrain(F)
+
+
+def gen_qv(unitcell_base, hkl=None):
+  """ generate the q vectors for given unitcell base """
+  pass
+
+
+if __name__ == "__main__":
+  # generate the testing distorsion
+  U, epsilon_ideal = gen_distorsion()
+
+  # generate a zero strain base
+  lc = [0.2965, 0.2965, 0.4747, 90, 90, 120]
+  B_perfect = get_base(lc)
+  # strain the perfect unit cell
+  B_strained = np.dot(U,B_perfect)
