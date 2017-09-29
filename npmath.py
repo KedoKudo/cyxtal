123d8ccef9da1a693c01bc1eb83b3fdbe908dc77
Chen <chenzhang8722@gmail.com>
2017-09-29 10:42:10 -0400

    directly use the norm from numpy
    realize there is no reason to make a wrapper on a function without provide any additional changes

--- npmath.py
+++ npmath.py
@@ -2,36 +2,28 @@
 # coding=utf-8
 
 # Functions commonly used but somehow still missing in standard Numpy
-# packages. 
+# packages.
 
 
 import numpy as np
+from numpy.linalg import norm
 
 
 def normalize(vec, axis=None):
     """
-    return a normalized vector/matrix 
-    axis=None : normalize entire vector/matrix 
+    return a normalized vector/matrix
+    axis=None : normalize entire vector/matrix
     axis=0    : normalize by column
     axis=1    : normalize by row
     """
     if axis is None:
-        return vec/np.linalg.norm(vec)
+        return vec/norm(vec)
     else:
-        return np.divide(vec, 
-                         np.tile(np.linalg.norm(vec, axis=axis), 
+        return np.divide(vec,
+                         np.tile(norm(vec, axis=axis),
                                  vec.shape[axis],
-                                ).reshape(vec.shape, 
+                                ).reshape(vec.shape,
                                           order='F' if axis==1 else 'C',
                                          )
                         )
 
-
-def norm(vec, axis=None):
-    """
-    return the norm of a vector/matrix using numpy.linalg.norm 
-    axis=None : norm of entire vector/matrix 
-    axis=0    : norm of each column
-    axis=1    : norm of each row
-    """
-    return np.linalg.norm(vec, axis=axis)
